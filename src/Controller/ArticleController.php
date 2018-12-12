c71a23d9c6ffca8f93ca5a7c611e39cfe0b72e46
Dmitriy Kirillov <dmitriy.kirillov@devellar.com>
2018-12-12 09:57:39 +0200

    Twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -19,6 +20,15 @@ class ArticleController
      */
     public function show($slag)
     {
-        return new Response(sprintf('Page: %s', $slag));
+        $comments = [
+            'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+            'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+            'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucfirst(str_replace('-', ' ', $slag)),
+            'comments' => $comments,
+        ]);
     }
 }
