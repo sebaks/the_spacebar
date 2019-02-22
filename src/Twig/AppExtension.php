15a0630b9e9bb72112b0889c8d217f0651bea8cd
Dmitriy Kirillov <dmitriy.kirillov@devellar.com>
2019-02-22 10:49:15 +0200

    Fun with Twig Extensions

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Twig;
+
+use App\Service\MarkdownHelper;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    /**
+     * @var MarkdownHelper
+     */
+    private $markdownHelper;
+
+
+    /**
+     * AppExtension constructor.
+     */
+    public function __construct(MarkdownHelper $markdownHelper)
+    {
+        $this->markdownHelper = $markdownHelper;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('cached_markdown', [$this, 'processMarkdown'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function processMarkdown($value)
+    {
+        return $this->markdownHelper->parse($value);
+    }
+}
