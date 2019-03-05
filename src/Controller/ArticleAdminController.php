f2222cae99eb3895430a3eea81bec78d1ae18086
Dmitriy Kirillov <dmitriy.kirillov@devellar.com>
2019-03-05 17:13:46 +0200

    Updating an Entity with New Fields

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -40,6 +40,10 @@ EOF
             $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
         }
 
+        $article->setAuthor('Mike Ferengi')
+            ->setHeartsCount(rand(5, 100))
+            ->setImageFilename('asteroid.jpeg');
+
         $entityManager->persist($article);
         $entityManager->flush();
 
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -36,6 +36,21 @@ class Article
      */
     private $publishedAt;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $heartsCount = 0;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imageFilename;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -87,5 +102,46 @@ class Article
         $this->publishedAt = $publishedAt;
 
         return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getHeartsCount(): ?int
+    {
+        return $this->heartsCount;
+    }
+
+    public function setHeartsCount(int $heartsCount): self
+    {
+        $this->heartsCount = $heartsCount;
+
+        return $this;
+    }
+
+    public function getImageFilename(): ?string
+    {
+        return $this->imageFilename;
+    }
+
+    public function setImageFilename(?string $imageFilename): self
+    {
+        $this->imageFilename = $imageFilename;
+
+        return $this;
+    }
+
+    public function getImagePath()
+    {
+        return 'images/' . $this->getImageFilename();
     }
 }
